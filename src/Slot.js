41505f4842c38f06fb9e9aeb1b399822e4d48a15
Florentino Carrera <florentinocarrera44@gmail.com>
2022-02-12 01:29:41 -0400

    Add style

--- src/Slot.js
+++ src/Slot.js
@@ -6,24 +6,24 @@ const minute = 60*second;
 class Slot extends React.Component {
   constructor(props) {
     super(props);
-    this.handleSelect = this.handleSelect.bind(this);
-    this.handleDeselect = this.handleDeselect.bind(this);
+    this.handleClick = this.handleClick.bind(this);
     this.state = {isSelected: false};
   }
 
-  handleSelect() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      0.2*minute
-    );
-    this.props.onCounterChange(1);
-    this.setState({isSelected: true});
-  }
-
-  handleDeselect() {
-    clearInterval(this.timerID);
-    this.props.onCounterChange(-1);
-    this.setState({isSelected: false});
+  handleClick() {
+    const isSelected = this.state.isSelected;
+    if (isSelected) {
+      clearInterval(this.timerID);
+      this.props.onCounterChange(-1);
+      this.setState({isSelected: false});
+    } else {
+      this.timerID = setInterval(
+        () => this.tick(),
+        0.2*minute
+      );
+      this.props.onCounterChange(1);
+      this.setState({isSelected: true});
+    }
   }
 
   tick() {
@@ -32,66 +32,18 @@ class Slot extends React.Component {
     this.setState({isSelected: false});
   }
 
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
   render() {
     const isSelected = this.state.isSelected;
-    let button;
-
-    if (isSelected) {
-      button = <SlotSelected onClick={this.handleDeselect} hour={this.props.hour} />;
-    } else {
-      button = <SlotEnabled onClick={this.handleSelect} hour={this.props.hour} />;
-    }
+    const modifier = isSelected ? 'Button-selected ' : '';
 
     return (
       <div>
-        {button}
-      </div>
-    );
-  }
-}
-
-class SlotDisabled extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleDeselect = this.handleDeselect.bind(this);
-  }
-
-  handleDeselect() {
-    this.props.onCounterChange(-1);
-  }
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleDeselect}>
-          Disabled {this.props.hour}
+        <button className={'Button ' + modifier + this.props.modifier} onClick={this.handleClick}>
+          {this.props.hour}
         </button>
       </div>
     );
   }
 }
 
-function SlotSelected(props) {
-  return (
-    <button onClick={props.onClick}>
-      Selected {props.hour}
-    </button>
-  );
-}
-
-function SlotEnabled(props) {
-  return (
-    <button onClick={props.onClick}>
-      Enabled {props.hour}
-    </button>
-  );
-}
-
-export {
-  Slot,
-  SlotDisabled
-};
+export default Slot;
--- src/Table.js
+++ src/Table.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import { Slot, SlotDisabled } from './Slot';
-
-const hours = ['8:00', '8:30', '9:00', '9:30', '10:00', '10:30', '11:00', '11:30', '12:00',
-               '12:30', '1:00', '1:30', '2:00', '2:30', '3:00', '3:30', '4:00', '4:30', '5:00', 
-               '5:30', '6:00', '6:30', '7:00', '7:30', '8:00'];
+import Slot from './Slot';
 
 class Table extends React.Component {
   constructor(props) {
@@ -19,19 +15,23 @@ class Table extends React.Component {
   }
 
   render() {
-    const counter = this.state.counter
+    const counter = this.state.counter;
+    let modifier = '';
 
-    const listItems = hours.map((hour, index) => {
-      if (counter >= 8) {
-        return <SlotDisabled key={index} hour={hour} onCounterChange={this.handleCounter} />
-      } else {
-        return <Slot key={index} hour={hour} onCounterChange={this.handleCounter} />
-      }
+    if (counter >= 8) {
+      modifier = 'Button-disable';
+    }
+
+    const listItems = [];
+
+    this.props.items.forEach((item) => {
+      listItems.push(
+        <Slot key={item.id} hour={item.hour} onCounterChange={this.handleCounter} modifier={modifier} />
+      );
     });
 
     return (
       <div>
-        Counter: {counter}
         {listItems}
       </div>
     );
